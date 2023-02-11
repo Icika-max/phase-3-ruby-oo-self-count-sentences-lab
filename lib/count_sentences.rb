985e9b90165c36717ec60b3e3b8ed450297a41a1
icika-max <icikakennedy@gmail.com>
2023-02-11 09:40:11 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
+      # Split the string into an array of substrings based on sentence delimiters
+      sentences = self.split(/\.|\?|!/)
+
+      # Remove any empty or whitespace-only substrings from the array
+      sentences.reject! { |s| s.strip.empty? }
+  
+      # Return the count of the remaining substrings
+      sentences.count
 
   end
 end
